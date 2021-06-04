60e5ab429c5ac7f4fc74fc5566e87764f7792111
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-06-04 14:48:17 +0300

    added login and register templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    context = {
+        'title': 'GeekShop - авторизация'
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'GeekShop - регистрация'
+    }
+    return render(request, 'users/register.html',context)
