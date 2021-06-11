c9ee1c3dad32a86686a1a6be9dfa8717b88724ee
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-06-12 01:21:57 +0300

    update methods for Basket model

--- baskets/models.py
+++ baskets/models.py
@@ -10,6 +10,10 @@ class Basket(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_baskets(self):
+        return Basket.objects.filter(user=self.user)
+
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
 
@@ -17,7 +21,7 @@ class Basket(models.Model):
         return self.quantity * self.product.price
 
     def total_sum(self):
-        return sum(basket.sum() for basket in Basket.objects.filter(user=self.user))
+        return sum(basket.sum() for basket in self.get_baskets)
 
     def total_quantity(self):
-        return sum(basket.quantity for basket in Basket.objects.filter(user=self.user))
+        return sum(basket.quantity for basket in self.get_baskets)
