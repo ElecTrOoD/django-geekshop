cd5d4ec9ea7ff5c52d3ecf01c5ab5856dcd483c9
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-06-12 05:33:02 +0300

    deleted commented code

--- baskets/views.py
+++ baskets/views.py
@@ -7,20 +7,6 @@ from baskets.models import Basket
 from products.models import Product
 
 
-# @login_required
-# def add_to_basket(request, product_id):
-#     product = Product.objects.get(id=product_id)
-#     baskets = Basket.objects.filter(user=request.user, product=product)
-#
-#     if not baskets.exists():
-#         Basket.objects.create(user=request.user, product=product, quantity=1)
-#         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-#     else:
-#         basket = baskets.first()
-#         basket.quantity += 1
-#         basket.save()
-#         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
 @login_required
 def add_to_basket(request, product_id):
     if request.is_ajax():
