e8157bd5ef70ea5e5cd3eeaae8abeb5d7dedced2
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-06-12 04:03:36 +0300

    added AJAX for products

--- baskets/views.py
+++ baskets/views.py
@@ -7,19 +7,36 @@ from baskets.models import Basket
 from products.models import Product
 
 
+# @login_required
+# def add_to_basket(request, product_id):
+#     product = Product.objects.get(id=product_id)
+#     baskets = Basket.objects.filter(user=request.user, product=product)
+#
+#     if not baskets.exists():
+#         Basket.objects.create(user=request.user, product=product, quantity=1)
+#         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+#     else:
+#         basket = baskets.first()
+#         basket.quantity += 1
+#         basket.save()
+#         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 @login_required
 def add_to_basket(request, product_id):
-    product = Product.objects.get(id=product_id)
-    baskets = Basket.objects.filter(user=request.user, product=product)
+    if request.is_ajax():
+        product = Product.objects.get(id=product_id)
+        baskets = Basket.objects.filter(user=request.user, product=product)
 
-    if not baskets.exists():
-        Basket.objects.create(user=request.user, product=product, quantity=1)
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-    else:
-        basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        if not baskets.exists():
+            Basket.objects.create(user=request.user, product=product, quantity=1)
+        else:
+            basket = baskets.first()
+            basket.quantity += 1
+            basket.save()
+        products = Product.objects.all()
+        context = {'products': products}
+        result = render_to_string('products/products_list.html', context)
+        return JsonResponse({'result': result})
 
 
 @login_required
