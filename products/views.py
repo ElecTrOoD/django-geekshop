71274bee3d0e0bc0f72d219f249b8d56249a2ff1
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-06-21 05:53:56 +0300

    added filtering products by category_id

--- products/views.py
+++ products/views.py
@@ -10,12 +10,14 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    goods = Product.objects.filter(is_active=True)
-    categories = ProductCategory.objects.all()
+def products(request, category_id=None):
+    if category_id:
+        goods = Product.objects.filter(is_active=True, category_id=category_id)
+    else:
+        goods = Product.objects.filter(is_active=True)
     context = {
         'title': 'GeekShop - Каталог',
-        'products': goods,
-        'categories': categories
+        'categories': ProductCategory.objects.all(),
+        'products': goods
     }
     return render(request, 'products/products.html', context)
