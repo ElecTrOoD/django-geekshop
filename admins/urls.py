c96397076f68e8f9f388f81207f363067c4b6bdb
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-06-21 09:30:21 +0300

    updated class names

--- admins/urls.py
+++ admins/urls.py
@@ -1,19 +1,22 @@
 from django.urls import path
 
-from admins.views import IndexView, UserListView, UserCreateView, UserUpdateView, UserDeleteView, UserCompleteDeleteView, ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView, ProductCompleteDeleteView
+from admins.views import AdminIndexView, AdminUserListView, AdminUserCreateView, AdminUserUpdateView, \
+    AdminUserDeleteView, AdminUserCompleteDeleteView, AdminProductListView, AdminProductCreateView, \
+    AdminProductUpdateView, AdminProductDeleteView, AdminProductCompleteDeleteView
 
 app_name = 'admins'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('users', UserListView.as_view(), name='admin_users'),
-    path('users/create/', UserCreateView.as_view(), name='admin_users_create'),
-    path('users/update/<int:pk>', UserUpdateView.as_view(), name='admin_users_update'),
-    path('users/delete/<int:pk>', UserDeleteView.as_view(), name='admin_users_delete'),
-    path('users/complete-delete/<int:pk>', UserCompleteDeleteView, name='admin_users_complete_delete'),
-    path('products', ProductListView.as_view(), name='admin_products'),
-    path('products/create/', ProductCreateView.as_view(), name='admin_products_create'),
-    path('products/update/<int:pk>', ProductUpdateView.as_view(), name='admin_products_update'),
-    path('products/delete/<int:pk>', ProductDeleteView.as_view(), name='admin_products_delete'),
-    path('products/complete-delete/<int:pk>', ProductCompleteDeleteView.as_view(), name='admin_products_complete_delete'),
+    path('', AdminIndexView.as_view(), name='index'),
+    path('users', AdminUserListView.as_view(), name='admin_users'),
+    path('users/create/', AdminUserCreateView.as_view(), name='admin_users_create'),
+    path('users/update/<int:pk>', AdminUserUpdateView.as_view(), name='admin_users_update'),
+    path('users/delete/<int:pk>', AdminUserDeleteView.as_view(), name='admin_users_delete'),
+    path('users/complete-delete/<int:pk>', AdminUserCompleteDeleteView, name='admin_users_complete_delete'),
+    path('products', AdminProductListView.as_view(), name='admin_products'),
+    path('products/create/', AdminProductCreateView.as_view(), name='admin_products_create'),
+    path('products/update/<int:pk>', AdminProductUpdateView.as_view(), name='admin_products_update'),
+    path('products/delete/<int:pk>', AdminProductDeleteView.as_view(), name='admin_products_delete'),
+    path('products/complete-delete/<int:pk>', AdminProductCompleteDeleteView.as_view(),
+         name='admin_products_complete_delete'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -11,26 +11,26 @@ from products.models import Product
 from users.models import User
 
 
-class IndexView(TemplateView):
+class AdminIndexView(TemplateView):
     template_name = 'admins/admin.html'
     extra_context = {'title': 'GeekShop - Админ'}
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(IndexView, self).dispatch(request, *args, **kwargs)
+        return super(AdminIndexView, self).dispatch(request, *args, **kwargs)
 
 
-class UserListView(ListView):
+class AdminUserListView(ListView):
     model = User
     template_name = 'admins/admin-users-read.html'
     extra_context = {'title': 'GeekShop - Админ | Пользователи'}
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserListView, self).dispatch(request, *args, **kwargs)
+        return super(AdminUserListView, self).dispatch(request, *args, **kwargs)
 
 
-class UserCreateView(CreateView):
+class AdminUserCreateView(CreateView):
     model = User
     template_name = 'admins/admin-users-create.html'
     form_class = UserAdminRegisterForm
@@ -39,10 +39,10 @@ class UserCreateView(CreateView):
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+        return super(AdminUserCreateView, self).dispatch(request, *args, **kwargs)
 
 
-class UserUpdateView(UpdateView):
+class AdminUserUpdateView(UpdateView):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
@@ -51,10 +51,10 @@ class UserUpdateView(UpdateView):
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
+        return super(AdminUserUpdateView, self).dispatch(request, *args, **kwargs)
 
 
-class UserDeleteView(DeleteView):
+class AdminUserDeleteView(DeleteView):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admin_users')
@@ -70,30 +70,30 @@ class UserDeleteView(DeleteView):
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
+        return super(AdminUserDeleteView, self).dispatch(request, *args, **kwargs)
 
 
-class UserCompleteDeleteView(DeleteView):
+class AdminUserCompleteDeleteView(DeleteView):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admin_users')
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(UserCompleteDeleteView, self).dispatch(request, *args, **kwargs)
+        return super(AdminUserCompleteDeleteView, self).dispatch(request, *args, **kwargs)
 
 
-class ProductListView(ListView):
+class AdminProductListView(ListView):
     model = Product
     template_name = 'admins/admin-products-read.html'
     extra_context = {'title': 'GeekShop - Админ | Продукты'}
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(ProductListView, self).dispatch(request, *args, **kwargs)
+        return super(AdminProductListView, self).dispatch(request, *args, **kwargs)
 
 
-class ProductCreateView(CreateView):
+class AdminProductCreateView(CreateView):
     model = Product
     template_name = 'admins/admin-products-create.html'
     form_class = ProductAdminForm
@@ -102,10 +102,10 @@ class ProductCreateView(CreateView):
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(ProductCreateView, self).dispatch(request, *args, **kwargs)
+        return super(AdminProductCreateView, self).dispatch(request, *args, **kwargs)
 
 
-class ProductUpdateView(UpdateView):
+class AdminProductUpdateView(UpdateView):
     model = Product
     template_name = 'admins/admin-products-update-delete.html'
     form_class = ProductAdminForm
@@ -114,24 +114,24 @@ class ProductUpdateView(UpdateView):
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(ProductUpdateView, self).dispatch(request, *args, **kwargs)
+        return super(AdminProductUpdateView, self).dispatch(request, *args, **kwargs)
 
 
-class ProductDeleteView(UserDeleteView):
+class AdminProductDeleteView(AdminUserDeleteView):
     model = Product
     template_name = 'admins/admin-products-update-delete.html'
     success_url = reverse_lazy('admins:admin_products')
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(ProductDeleteView, self).dispatch(request, *args, **kwargs)
+        return super(AdminProductDeleteView, self).dispatch(request, *args, **kwargs)
 
 
-class ProductCompleteDeleteView(DeleteView):
+class AdminProductCompleteDeleteView(DeleteView):
     model = Product
     template_name = 'admins/admin-products-update-delete.html'
     success_url = reverse_lazy('admins:admin_products')
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
-        return super(ProductCompleteDeleteView, self).dispatch(request, *args, **kwargs)
+        return super(AdminProductCompleteDeleteView, self).dispatch(request, *args, **kwargs)
