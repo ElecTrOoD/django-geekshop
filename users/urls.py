4e54eaadc13883cf1b28a689b3d071ac5a860a19
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-06-23 08:19:13 +0300

    added class-based views to users app

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from users.views import login, register,profile, logout
+from users.views import UserLoginView, UserLogoutView, UserRegisterView, UserProfileUpdateView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('profile/', profile, name='profile'),
-    path('logout/', logout, name='logout'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('profile/', UserProfileUpdateView.as_view(), name='profile'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
