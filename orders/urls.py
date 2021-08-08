a310c055c7a621761eab50ab3d6115507de75021
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-08-09 02:49:54 +0300

    created orders app

---
+++ orders/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from orders.views import OrderList, OrderItemCreate, OrderItemUpdate, OrderItemDelete, OrderItemRead, \
+    confirm_order_forming
+
+app_name = 'orders'
+
+urlpatterns = [
+    path('', OrderList.as_view(), name='orders_list'),
+    path('create/', OrderItemCreate.as_view(), name='orders_create'),
+    path('<pk>/', OrderItemRead.as_view(), name='orders_read'),
+    path('update/<pk>/', OrderItemUpdate.as_view(), name='orders_update'),
+    path('delete/<pk>/', OrderItemDelete.as_view(), name='orders_delete'),
+    path('confirm/<pk>/', confirm_order_forming, name='orders_confirm'),
+]
