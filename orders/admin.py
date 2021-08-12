65e3de7062c920a620a3cade9b82ec6f0f9abcb9
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-08-12 19:54:10 +0300

    customized django-admin for orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order, OrderItem
+
+
+class OrderItemsAdminInline(admin.TabularInline):
+    model = OrderItem
+    fields = ('order', 'product', 'quantity')
+    extra = 0
+
+
+@admin.register(Order)
+class OrdersAdmin(admin.ModelAdmin):
+    fields = ('user', 'status', 'is_active', 'created', 'updated',)
+    list_display = ('__str__', 'user', 'status', 'is_active', 'created', 'updated',)
+    readonly_fields = ('created', 'updated',)
+    inlines = (OrderItemsAdminInline,)
+    can_delete = False
