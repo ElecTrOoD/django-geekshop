eb516c7ea950df266c9cc97ab0f18c8b217c1b18
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-08-21 06:52:42 +0300

    added login_required()

--- orders/urls.py
+++ orders/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from orders.views import OrderList, OrderItemCreate, OrderItemUpdate, OrderItemDelete, OrderItemRead, \
@@ -6,11 +7,11 @@ from orders.views import OrderList, OrderItemCreate, OrderItemUpdate, OrderItemD
 app_name = 'orders'
 
 urlpatterns = [
-    path('', OrderList.as_view(), name='orders_list'),
-    path('create/', OrderItemCreate.as_view(), name='orders_create'),
-    path('<pk>/', OrderItemRead.as_view(), name='orders_read'),
-    path('update/<pk>/', OrderItemUpdate.as_view(), name='orders_update'),
-    path('delete/<pk>/', OrderItemDelete.as_view(), name='orders_delete'),
-    path('confirm/<pk>/', confirm_order_forming, name='orders_confirm'),
-    path('product/<pk>/price/', product_price, name='product_price'),
+    path('', login_required(OrderList.as_view()), name='orders_list'),
+    path('create/', login_required(OrderItemCreate.as_view()), name='orders_create'),
+    path('<pk>/', login_required(OrderItemRead.as_view()), name='orders_read'),
+    path('update/<pk>/', login_required(OrderItemUpdate.as_view()), name='orders_update'),
+    path('delete/<pk>/', login_required(OrderItemDelete.as_view()), name='orders_delete'),
+    path('confirm/<pk>/', login_required(confirm_order_forming), name='orders_confirm'),
+    path('product/<pk>/price/', login_required(product_price), name='product_price'),
 ]
