6db9df0ca01df50786ad0fda919890564b20fbb1
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2021-09-01 04:41:28 +0300

    minor fix

--- admins/forms.py
+++ admins/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 
 from products.models import Product, ProductCategory
-from users.forms import UserRegisterForm, UserProfileForm
+from users.forms import UserRegisterForm
 from users.models import User
 
 
@@ -14,7 +14,7 @@ class UserAdminRegisterForm(UserRegisterForm):
         fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
 
 
-class UserAdminProfileForm(UserProfileForm):
+class UserAdminProfileForm(ModelForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4'}))
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
